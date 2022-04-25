065b7dbb5e5d069aca912ade77bed5a8970fb312
ROTTIEBONES <rottiebones@gmail.com>
2022-04-24 23:02:30 -0400

    added motion state triggers for climbing walking swimming etc

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
+    public enum State
+    {
+        idle,
+        walking,
+        swimming,
+        climbing
+    }
+
+    public State currentState = State.idle;
+    Vector3 lastPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch(currentState)
+        {
+            case State.idle: Idle(); break;
+            case State.walking: Walking(); break;
+            case State.swimming: Swimming(); break;
+            case State.climbing: Climbing(); break;
+            default: break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
         
+    }
+
+    void Swimming()
+    {
+        Debug.Log("I am Swimming");
+    }
+
+    void Climbing()
+    {
+        Debug.Log("I am Climbing");
+    }
+
+    void Idle()
+    {
+        Debug.Log("I am idle");
+        if(lastPosition != transform.position)
+        {
+            Debug.Log("Moving");
+        }
+        lastPosition = transform.position;
+    }
+
+    void Walking()
+    {
+        Debug.Log("I am walking");
+        if (lastPosition == transform.position)
+        {
+            Debug.Log("Moving");
+        }
+        lastPosition = transform.position;
     }
 }
